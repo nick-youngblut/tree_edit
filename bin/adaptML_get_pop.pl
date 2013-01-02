71a1668bbe97a9c037f7dc1dca3a0b6dba99a23c
Nick Youngblut <nyoungb2@gmail.com>
2013-01-02 10:48:59 -0600

    adaptML_get_pop: changing monophyl algorithm

--- bin/adaptML_get_pop.pl
+++ bin/adaptML_get_pop.pl
@@ -7,21 +7,143 @@ use Pod::Usage;
 use Data::Dumper;
 use Getopt::Long;
 use File::Spec;
+use Bio::TreeIO;
 
 ### args/flags
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
-my ($verbose);
+my ($verbose, $tree_in, $habitat_in, $full_in, $format, $abund_cut);
 GetOptions(
-	   "tree=s" => \$tree_in,
-	   "format=s" => \$format,
+	   "tree=s" => \$tree_in,		# tree file
+	   "habitat=s" => \$habitat_in,	# habitat file (2column)
+	   "format=s" => \$format,		# tree format
+	   "cutoff=s" => \$abund_cut,	# abundance cutoff for filtering habitat
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
 	   );
 
 ### I/O error & defaults
+die " ERROR: Provide tree file (newick or nexus format)." if ! $tree_in;
+if(! $format){ $format = "newick"; }
+$format = check_format($format);
+die " ERROR: Provide a habitat file (from AdaptML)." if ! $habitat_in;
+$abund_cut = 20 if ! $abund_cut;
 
 ### MAIN
+my $treeo = tree_io($tree_in, $format);
+my $habs_ref = load_habitat($habitat_in);
+$treeo = find_pops($treeo, $habs_ref);
+#print_node_ids($treeo, "true", "true");
+$habs_ref = filter_habs($habs_ref, $abund_cut);				# filtering by abundance
+exit;
+#get_leaf_branch_lengths($treeo);
+
+### Subroutines
+sub find_pops{
+	my ($treeo, $habs_ref) = @_;
+	
+	my %skipped;
+	my %pop_nodes;
+	for my $node ($treeo->get_nodes){		# foreach internal node
+		next if $node->is_Leaf;
+		
+		# getting all leaves #
+		my @children;
+		for my $child ($node->get_all_Descendents){
+			next if ! $child->is_Leaf;
+			push(@children, $child);
+			}
+		
+		# getting all leaf habitats #
+		my %node_habs;
+		foreach my $child (@children){
+			if (! exists $$habs_ref{$child->id}){
+				if (! exists $skipped{$child->id}){
+					print STDERR " WARNING: ", $child->id, " not found in habitat file; skipping\n";
+					}
+				else{
+					$skipped{$child->id} = 1;
+					}
+				next;
+				}	
+			$node_habs{$$habs_ref{$child->id}}++;
+			}
+		
+		# filtering #
+		foreach my $key (keys %node_habs){
+			delete $node_habs{$key} if $node_habs{$key} < $abund_cut;
+			}
+		
+		# checking for monophyl; labeling all decendent nodes #
+		if(scalar keys %node_habs == 1){
+			
+			}
+		$pop_nodes{$node->
+		}
+	}
+
+sub print_node_ids{
+	# error checking
+	my ($tree, $leaf, $int) = @_;
+	if($leaf =~ /true/i){
+		foreach my $node ($tree->get_leaf_nodes){
+			print "leaf: ", $node->id, "\n";
+			}
+		}
+	if($int =~ /true/i){
+		foreach my $node ($tree->get_nodes){
+			if($node->is_Leaf == 0){
+				print "internal: ", $node->id, "\n";
+				}
+			}
+		}
+	}
+
+sub filter_habs{
+# filtering by abundance cutoff #
+	my ($habs_ref, $abund_cut) = @_;
+	
+	# making a hab count table #
+	my %hab_cnt;
+	map{$hab_cnt{$_}++ } values %$habs_ref; 
+
+	print Dumper %hab_cnt; exit;
+	}
+
+
+sub check_format{
+# checking format input #
+	my $format = shift;
+	if($format =~ /^new/i){ $format = "newick"; }
+	elsif($format =~ /^nex/i){ $format = "nexus"; }
+	else{ die " ERROR: '$format' format not recognized (newick | nexus)\n"; }
+	return $format;
+	}
+
+sub tree_io{
+	# loading tree object #
+	my ($tree_in, $format) = @_;
+	my $input = Bio::TreeIO -> new(-file => $tree_in,
+								-format => $format);
+	my $treeio = $input->next_tree;	
+	return $treeio;
+	}
+
+sub load_habitat{
+## loading habitat file ##
+	my $habitat_in = shift;
+	open IN, $habitat_in or die $!;
+	my %habs;
+	while(<IN>){
+		chomp;
+		s/#.+//;
+		next if /^\s$/;
+		my @line = split /\t/;
+		die " ERROR: habitat file not formated correctly\n" if scalar @line != 2;
+		$habs{$line[0]} = $line[1];
+		}
+	return \%habs;
+	}
 
 
 __END__
