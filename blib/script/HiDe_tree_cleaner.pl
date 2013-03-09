ecfe025ad002a9d768cfbf40068405043dd4b6c0
Nick Youngblut <nyoungb2@illinois.edu>
2013-03-09 13:44:53 -0600

    build commit

--- blib/script/HiDe_tree_cleaner.pl
+++ blib/script/HiDe_tree_cleaner.pl
@@ -12,61 +12,184 @@ use Bio::TreeIO;
 ### args/flags
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
-my ($verbose, @gene_in, $species_in, $tformat, $species, $multi);
+my ($verbose, @gene_in, $species_in, $tformat, $species, $multi, $index_in, $HiDe_in);
 GetOptions(
-	   "gene=s{,}" => \@gene_in,
+	   "gene=s{,}" => \@gene_in,		# gene trees
 	   "format=s" => \$tformat,
-	   "species=s" => \$species_in,		# species tree?
-	   #"multi" => \$multi, 			# multiple trees?
+	   "species=s" => \$species_in,		# species tree
+	   "index=s" => \$index_in, 		# index file
+	   "out=s" => \$HiDe_in, 			# hide input
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
 	   );
 
 ### I/O error & defaults
-die " Provide a species & >=1 gene tree file (newick or nexus).\n" 
+# if HiDe output provided #
+HiDe_out_change_names($HiDe_in, $index_in) if $HiDe_in;
+
+die " ERROR: Provide a species & >=1 gene tree file (newick or nexus).\n" 
 	if ! @gene_in || ! $species_in;
 $tformat = check_tree_format($tformat);
 
-#my $outfile;
-#if($species){ $outfile = "species.newick"; }
-#else{ ($outfile = $gene_in) =~ s/\.[^\.]+$|$/_cln.nwk/; } 
-#unlink($outfile) if -e $outfile;
 
 ### MAIN
 # loading species tree names #
-#my $treeio = tree_io($gene_in, $tformat);
-check_names($species_in, \@gene_in, $tformat);
-	exit;
+my $names_r = check_names($species_in, \@gene_in, $tformat);
+
+# making name index #
+my $nameI_r = make_name_index($names_r, $species_in);
+
 
 # editting and writing #
+clean_tree($species_in, $tformat, $nameI_r, "species.newick");
 foreach my $gene_in (@gene_in){
-my $treeio = tree_io($gene_in, $tformat);
-my $out = new Bio::TreeIO(-fh => \*STDOUT, -format => "newick");
-for my $treeo ($treeio->next_tree){
-
-	$treeo = edit_leaf_labels($treeo);
-	$treeo = remove_node_labels($treeo);
-	$treeo = remove_bootstrap($treeo);
-	$treeo = remove_branch_length($treeo);
-	$treeo = check_tree_cleaning($treeo) if $verbose;
-
-		#print Dumper $treeo; exit;
-	$out->write_tree($treeo);
+	clean_tree($gene_in, $tformat, $nameI_r);
 	}
-}
 
-#print STDERR " Newick tree file written:\n  $outfile\n";
+
+# main subroutines #
+sub clean_tree{
+	my ($gene_in, $tformat, $nameI_r, $outfile) = @_;
+	
+	# I/O #
+	($outfile = $gene_in) =~ s/\.[^\.]+$|$/_cln.newick/ unless $outfile;
+	unlink $outfile if -e $outfile;
+	
+	my $treeio = Bio::TreeIO -> new(-file => $gene_in,
+								-format => $tformat);
+	my $out = new Bio::TreeIO(-file => ">>$outfile", -format => "newick");
+	
+	# cleaning trees #
+	while (my $treeo = $treeio->next_tree){
+		$treeo = edit_leaf_labels($treeo, $nameI_r);
+		$treeo = remove_node_labels($treeo);
+		$treeo = remove_bootstrap($treeo);
+		$treeo = remove_branch_length($treeo);
+		$treeo = check_tree_cleaning($treeo) if $verbose;
+		
+		$out->write_tree($treeo);
+		}
+	
+	# removing all colons (from branch lengths #
+	`perl -pi -e 's/://g; s/;/;\n/g' $outfile`;	
+	
+	print STDERR "...Cleaned tree written: $outfile\n";
+	}
+
+sub HiDe_out_change_names{
+	my ($HiDe_in, $index_in) = @_;
+	die " ERROR: provide the HiDe output and the index file (*NI.txt).\n"
+		if ! $HiDe_in || ! $index_in;
+	
+	my $nameI_r = load_name_index($index_in);
+	
+	(my $outfile = $HiDe_in) =~ s/\.[^\.]+$|$/_rn.txt/;
+	open IN, $HiDe_in or die $!;
+	open OUT, ">$outfile" or die $!;
+	while(<IN>){
+		chomp;
+		my @line = split /\t| --> /;
+		foreach my $cat (@line[1..2]){
+			if($cat =~ /LCA/){					# if LCA of 2 taxa
+				my @parts = split /[(,)]/, $cat;
+				foreach my $index (@parts[1..2]){
+					die " ERROR: '$index' not found in name index!\n" 
+						unless exists $nameI_r->{$index};
+					$index = $nameI_r->{$index}
+					}
+				$cat = "$parts[0]($parts[1],$parts[2])";
+				}
+			else{					# if taxon
+				my @parts = split /-/, $cat;
+				die " ERROR: '$parts[1]' not found in name index!\n" 
+						unless exists $nameI_r->{$parts[1]};
+				$parts[1] = $nameI_r->{$parts[1]};
+				$cat = "$parts[0]-$parts[1]";
+				}
+			}
+		if($line[3]){
+			print OUT join("\t", $line[0], join(" --> ", @line[1..2]), $line[3]), "\n";
+			}
+		else{
+			print OUT join("\t", $line[0], join(" --> ", @line[1..2])), "\n";
+			}
+		}
+	close IN;
+	close OUT;
+	
+	
+	print STDERR "...New HiDe file written: $outfile\n";
+	exit;
+	}
 
 ### Subroutines
+sub load_name_index{
+	my ($index_in) = @_;
+	open IN, $index_in or die $!;
+	my %nameI;
+	while(<IN>){
+		chomp;
+		my @line = split /\t/;
+		$nameI{$line[0]} = $line[1];
+		}
+	close IN;
+	return \%nameI;		# %{index}=>{name}
+	}
+
+sub make_name_index{
+# making a name index for conversion of names post HiDe #
+	my ($names_r, $species_in) = @_;
+
+	# making index #
+	my %nameI;
+	for my $i (0..$#$names_r){
+		$nameI{$$names_r[$i]} = $i;
+		}
+
+	# writing out index #
+	(my $outfile = $species_in) =~ s/\.[^\.]+$|$/_NI.txt/;
+	open OUT, ">$outfile" or die $!;
+	foreach my $name (sort{$nameI{$a}<=>$nameI{$b}} keys %nameI){
+		print OUT join("\t", $nameI{$name} + 1, $name), "\n";
+		}
+	close OUT;
+	
+	print STDERR "...name index file written: $outfile\n";
+	
+	return \%nameI;
+	}
+
 sub check_names{
 	my ($species_in, $gene_in_r, $tformat) = @_;
+
+	print STDERR "...checking for matching names in species and gene trees\n";
+	my %names;
+
+	# species tree #
 	open IN, "tree_get_names.pl -t $species_in -f $tformat | " or die $!;
 	while(<IN>){
-		print Dumper $_;
+		chomp;
+		my @line = split /\t/;
+		$names{$line[0]}++;
+		die " ERROR in species tree: $line[0] found twice\n" if $names{$line[0]} > 1;
 		}
 	close IN;
 	
+	# gene trees #
+	foreach my $gene_in (@$gene_in_r){
+		open IN, "tree_get_names.pl -t $gene_in -f $tformat | " or die $!;
+		while(<IN>){
+			chomp;
+			my @line = split /\t/;
+			die " ERROR in $gene_in: $line[0] not found in species tree\n" unless
+				exists $names{$line[0]};
+			}
+		close IN;	
+		}
 
+	print STDERR "...names match!\n";
+	
+	return [keys %names];
 	}
 
 sub remove_branch_length{
@@ -78,11 +201,13 @@ sub remove_branch_length{
 	}
 
 sub edit_leaf_labels{
-	my ($treeo) = @_;
+	my ($treeo, $nameI_r) = @_;
 	for my $node ($treeo->get_nodes){
 		next unless $node->is_Leaf;
-		(my $id = $node->id) =~ s/[_.+-]/X/g;
-		$node->id($id);
+		my $node_id = $node->id;
+		die " ERROR: $node_id not found in name index!\n" unless
+			exists $nameI_r->{$node_id};
+		$node->id($nameI_r->{$node_id} + 1);			# converting IDs to numbers
 		}
 	return $treeo;
 	}
@@ -128,14 +253,6 @@ sub check_tree_format{
 	return $format;
 	}
 
-sub tree_io{
-	# loading tree object #
-	my ($gene_in, $format) = @_;
-	my $treeio = Bio::TreeIO -> new(-file => $gene_in,
-								-format => $format);
-	return $treeio;
-	}
-
 
 __END__
 
@@ -143,15 +260,31 @@ __END__
 
 =head1 NAME
 
-template.pl -- script template
+HiDe_tree_cleaner.pl -- Cleaning trees for HiDe analysis (score.lua)
 
 =head1 SYNOPSIS
 
-template.pl [options] < input > output
+=head2 Pre-HiDe
+
+HiDe_tree_cleaner.pl -s -g [-f] [-v]
+
+=head2 Post-HiDe
+
+HiDe_tree_cleaner.pl -i -o
 
 =head2 options
 
 =over
+
+=item -s 	Species tree (newick or nexus).
+
+=item -g 	>=1 gene/LCB tree (newick or nexus).
+
+=item -f 	Format of trees (newick or nexus). [newick]
+
+=item -i 	Index file.
+
+=item -o 	score.lua output.
 
 =item -v	Verbose output
 
@@ -161,24 +294,48 @@ template.pl [options] < input > output
 
 =head2 For more information:
 
-perldoc template.pl
+perldoc HiDe_tree_cleaner.pl
 
 =head1 DESCRIPTION
 
-The flow of execution is roughly:
-   1) Step 1
-   2) Step 2
-   3) Step 3
+HiDe (eg score.lua) requires very simple trees for input.
+This script removes:
+
+=over
+
+=item * 	Branch lengths
+
+=item * 	Internal node labels
+
+=item * 	Bootstrap values
+
+=back
+
+It also coververts all leaf names to numbers. The index is saved to "*_NI.txt"
+
+Leaf names must match among species and gene trees!
+
+=head2 Requires:
+
+=over
+
+=item * 	tree_get_names.pl
+
+=back
 
 =head1 EXAMPLES
 
-=head2 Usage method 1
+=head2 Usage: pre-HiDe
 
-template.pl <read1.fastq> <read2.fastq> <output directory or basename>
+HiDe_tree_cleaner.pl -s species.nwk -g gene*.newick
 
-=head2 Usage method 2
+=head2 Usage: HiDe
 
-template.pl <library file> <output directory or basename>
+score.lua . 80 > score.lua_out.txt
+
+=head2 Usage: post-HiDe
+
+HiDe_tree_cleaner.pl -o score.lua_out.txt -i species_NI.txt
 
 =head1 AUTHOR
 
@@ -186,7 +343,7 @@ Nick Youngblut <nyoungb2@illinois.edu>
 
 =head1 AVAILABILITY
 
-sharchaea.life.uiuc.edu:/home/git/
+sharchaea.life.uiuc.edu:/home/git/tree_edit/
 
 =head1 COPYRIGHT
 
